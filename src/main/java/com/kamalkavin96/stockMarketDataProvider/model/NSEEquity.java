9e88e7cd7f1a477d92a46a408097ba514b5da4dc
kamalkavin96 <kamalkavin68@gmail.com>
2025-10-09 10:29:29 +0530

    Add NSEEquity entity and configure application properties

---
+++ src/main/java/com/kamalkavin96/stockMarketDataProvider/model/NSEEquity.java
@@ -0,0 +1,37 @@
+package com.kamalkavin96.stockMarketDataProvider.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "nseequity_v1")
+public class NSEEquity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(unique = true, nullable = false, length = 100, name = "symbol")
+    private String symbol;
+    @Column(name = "name_of_company", nullable = false)
+    private String nameOfCompany;
+    @Column(name = "series", nullable = false)
+    private String series;
+    @Column(name = "date_of_listing", nullable = false)
+    private LocalDate dateOfListing;
+    @Column(name = "paid_up_value", nullable = false)
+    private int paidUpValue;
+    @Column(name = "market_lot", nullable = false)
+    private int marketLot;
+    @Column(name = "isin_number", nullable = false)
+    private String isinNumber;
+    @Column(name = "face_value", nullable = false)
+    private int faceValue;
+    @Column(name = "issued_size", nullable = false)
+    private Long issuedSize;
+}
