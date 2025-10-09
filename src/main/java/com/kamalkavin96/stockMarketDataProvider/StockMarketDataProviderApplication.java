400cc6f4313ce157f8e87d5a397b0d804665dc1d
kamalkavin96 <kamalkavin68@gmail.com>
2025-10-09 09:37:12 +0530

    initial commit

---
+++ src/main/java/com/kamalkavin96/stockMarketDataProvider/StockMarketDataProviderApplication.java
@@ -0,0 +1,13 @@
+package com.kamalkavin96.stockMarketDataProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketDataProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketDataProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamalkavin96/stockMarketDataProvider/StockMarketDataProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamalkavin96.stockMarketDataProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketDataProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
